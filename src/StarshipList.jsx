6901df249dc5b8ae1d6b6eea65c063cf36f43cc1
Kevin <kevin.rodriguez678@gmail.com>
2022-07-07 13:04:00 -0400

    add components and service folder

---
+++ src/StarshipList.jsx
@@ -0,0 +1,6 @@
+import { useState, useEffect } from "react";
+import {getStarshipList} from './services/sw-api'
+
+// const StarshipList = () => {
+//   const fetchStarshipList = async
+// }
---
+++ src/componets/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+// import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <header className="app-header">
+      <h1>Star Wars</h1>
+    </header>
+  )
+}
+
+export default NavBar
